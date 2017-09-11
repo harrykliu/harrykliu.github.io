599606bf01d69cc118c978a33b955d369e236a52
Harry Liu <hkl6@duke.edu>
2017-09-11 17:20:11 -0400

    Add slideshow to display homepage summary

---
+++ slideshow.js
@@ -0,0 +1,34 @@
+var slideIndex = 1;
+showSlides(slideIndex); // initial slide shown
+
+/* move to next or previous slide */
+function switchSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+/* when dot is clicked, change to selected slide */
+function currentSlide(n) {
+  showSlides(slideIndex = n); 
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+      slideIndex = 1;
+  } 
+  if (n < 1) {
+      slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+    
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+
+}
