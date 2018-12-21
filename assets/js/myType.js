ad2d078dec366a17f0c714e53091621785ab72d6
Harry Liu <hkl6@duke.edu>
2018-12-21 16:14:03 -0500

    Update Typed Content

--- assets/js/myType.js
+++ assets/js/myType.js
@@ -1,7 +1,7 @@
 //typed js
     $(".typed").typed({
         strings: ["My name is Harry", 
-        			"I'm an Economics & CS Junior @ Duke", 
+        			"I'm an Economics & CS Senior @ Duke", 
         			"Welcome to my site!"],
         typeSpeed: 60,
         backDelay: 1000,
